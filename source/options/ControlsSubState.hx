bf9b5030310f5145f251e9fafc99ac284e761aa6
TheLeerName <lesha.nechaev.05@mail.ru>
2022-06-30 23:01:22 +0700

    shut up

--- source/options/ControlsSubState.hx
+++ source/options/ControlsSubState.hx
@@ -306,7 +306,7 @@ class ControlsSubState extends MusicBeatSubstate {
 			item.destroy();
 		}
 
-		trace('Reloaded keys: ' + ClientPrefs.keyBinds);
+		//trace('Reloaded keys: ' + ClientPrefs.keyBinds);
 
 		for (i in 0...grpOptions.length) {
 			if(!unselectableCheck(i, true)) {
